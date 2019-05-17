23ef1ac23e79c576cc65fef78c2c29914b3a5499
andrelandgraf <andre.timo.landgraf@gmail.com>
2019-05-17 11:40:33 +0200

    tested ask pipeline, tested hello world intent

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -2,239 +2,25 @@
 /* eslint no-use-before-define: 0 */
 // sets up dependencies
 const Alexa = require('ask-sdk-core');
-const i18n = require('i18next');
-const sprintf = require('i18next-sprintf-postprocessor');
 
-// core functionality for fact skill
-const HelloWorldHandler = {
-  canHandle(handlerInput) {
-    const request = handlerInput.requestEnvelope.request;
-    // checks request type
-    return request.type === 'LaunchRequest'
-      || (request.type === 'IntentRequest'
-        && request.intent.name === 'HelloWorldIntent');
-  },
-  handle(handlerInput) {
-    const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
-    const speakOutput = requestAttributes.t('HELLO_WORLD_MESSAGE');
+const builtInHandlers = require( './intentHandlers/builtInHandler' );
+const errorHandlers = require( './intentHandlers/errorHandler' );
+const helloWorldHandlers = require( './intentHandlers/helloWorld' );
 
-    return handlerInput.responseBuilder
-      .speak(speakOutput)
-      .withSimpleCard(requestAttributes.t('SKILL_NAME'), speakOutput)
-      .getResponse();
-  },
-};
-
-const HelpHandler = {
-  canHandle(handlerInput) {
-    const request = handlerInput.requestEnvelope.request;
-    return request.type === 'IntentRequest'
-      && request.intent.name === 'AMAZON.HelpIntent';
-  },
-  handle(handlerInput) {
-    const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
-    return handlerInput.responseBuilder
-      .speak(requestAttributes.t('HELP_MESSAGE'))
-      .reprompt(requestAttributes.t('HELP_REPROMPT'))
-      .getResponse();
-  },
-};
-
-const FallbackHandler = {
-  // 2018-Aug-01: AMAZON.FallbackIntent is only currently available in en-* locales.
-  //              This handler will not be triggered except in those locales, so it can be
-  //              safely deployed for any locale.
-  canHandle(handlerInput) {
-    const request = handlerInput.requestEnvelope.request;
-    return request.type === 'IntentRequest'
-      && request.intent.name === 'AMAZON.FallbackIntent';
-  },
-  handle(handlerInput) {
-    const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
-    return handlerInput.responseBuilder
-      .speak(requestAttributes.t('FALLBACK_MESSAGE'))
-      .reprompt(requestAttributes.t('FALLBACK_REPROMPT'))
-      .getResponse();
-  },
-};
-
-const ExitHandler = {
-  canHandle(handlerInput) {
-    const request = handlerInput.requestEnvelope.request;
-    return request.type === 'IntentRequest'
-      && (request.intent.name === 'AMAZON.CancelIntent'
-        || request.intent.name === 'AMAZON.StopIntent');
-  },
-  handle(handlerInput) {
-    const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
-    return handlerInput.responseBuilder
-      .speak(requestAttributes.t('STOP_MESSAGE'))
-      .getResponse();
-  },
-};
-
-const SessionEndedRequestHandler = {
-  canHandle(handlerInput) {
-    const request = handlerInput.requestEnvelope.request;
-    return request.type === 'SessionEndedRequest';
-  },
-  handle(handlerInput) {
-    console.log(`Session ended with reason: ${handlerInput.requestEnvelope.request.reason}`);
-    return handlerInput.responseBuilder.getResponse();
-  },
-};
-
-const ErrorHandler = {
-  canHandle() {
-    return true;
-  },
-  handle(handlerInput, error) {
-    console.log(`Error handled: ${error.message}`);
-    console.log(`Error stack: ${error.stack}`);
-    const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
-    return handlerInput.responseBuilder
-      .speak(requestAttributes.t('ERROR_MESSAGE'))
-      .reprompt(requestAttributes.t('ERROR_MESSAGE'))
-      .getResponse();
-  },
-};
-
-const LocalizationInterceptor = {
-  process(handlerInput) {
-    // Gets the locale from the request and initializes 
-    // i18next.
-    const localizationClient = i18n.use(sprintf).init({
-      lng: handlerInput.requestEnvelope.request.locale,
-      resources: languageStrings,
-    });
-    // Creates a localize function to support arguments.
-    localizationClient.localize = function localize() {
-      // gets arguments through and passes them to
-      // i18next using sprintf to replace string placeholders
-      // with arguments.
-      const args = arguments;
-      const values = [];
-      for (let i = 1; i < args.length; i += 1) {
-        values.push(args[i]);
-      }
-      const value = i18n.t(args[0], {
-        returnObjects: true,
-        postProcess: 'sprintf',
-        sprintf: values,
-      });
-
-      // If an array is used then a random value is selected 
-      if (Array.isArray(value)) {
-        return value[Math.floor(Math.random() * value.length)];
-      }
-      return value;
-    };
-    // this gets the request attributes and save the localize function inside 
-    // it to be used in a handler by calling requestAttributes.t(STRING_ID, [args...])
-    const attributes = handlerInput.attributesManager.getRequestAttributes();
-    attributes.t = function translate(...args) {
-      return localizationClient.localize(...args);
-    };
-  },
-};
+const internationalization = require( './internationalization/internatioalization' );
 
 const skillBuilder = Alexa.SkillBuilders.custom();
 
 exports.handler = skillBuilder
   .addRequestHandlers(
-    HelloWorldHandler,
-    HelpHandler,
-    ExitHandler,
-    FallbackHandler,
-    SessionEndedRequestHandler,
+    helloWorldHandlers.HelloWorldHandler,
+    builtInHandlers.HelpHandler,
+    builtInHandlers.ExitHandler,
+    builtInHandlers.FallbackHandler,
+    builtInHandlers.SessionEndedRequestHandler,
   )
-  .addRequestInterceptors(LocalizationInterceptor)
-  .addErrorHandlers(ErrorHandler)
+  .addRequestInterceptors( internationalization.LocalizationInterceptor )
+  .addErrorHandlers( errorHandlers.ErrorHandler )
   .lambda();
 
 
-// translations
-const deData = {
-  translation: {
-    SKILL_NAME: 'Foodoo',
-    HELP_MESSAGE: 'Du kannst hallo sagen, oder du kannst „Beenden“ sagen... Wie kann ich dir helfen?',
-    HELP_REPROMPT: 'Wie kann ich dir helfen?',
-    FALLBACK_MESSAGE: 'Die Weltraumfakten Skill kann dir dabei nicht helfen. Sie kann dir Fakten über den Raum erzählen, wenn du dannach fragst.',
-    FALLBACK_REPROMPT: 'Wie kann ich dir helfen?',
-    ERROR_MESSAGE: 'Es ist ein Fehler aufgetreten.',
-    STOP_MESSAGE: 'Auf Wiedersehen!',
-    HELLO_WORLD_MESSAGE:
-      [
-        'Hello World!',
-        'Hallo Welt!',
-        'Guten Tag, liebe Welt!',
-      ],
-  },
-};
-
-const dedeData = {
-  translation: {
-    SKILL_NAME: 'Foodoo auf Deutsch',
-  },
-};
-
-const enData = {
-  translation: {
-    SKILL_NAME: 'Foodoo',
-    HELP_MESSAGE: 'You can say hi, or, you can say exit... What can I help you with?',
-    HELP_REPROMPT: 'What can I help you with?',
-    FALLBACK_MESSAGE: 'The Foodoo skill can\'t help you with that.  It can help you discover facts about space if you say tell me a space fact. What can I help you with?',
-    FALLBACK_REPROMPT: 'What can I help you with?',
-    ERROR_MESSAGE: 'Sorry, an error occurred.',
-    STOP_MESSAGE: 'Goodbye!',
-    HELLO_WORLD_MESSAGE:
-      [
-        'Hello World!',
-        'Hey World!',
-        'Hello my little World!',
-      ],
-  },
-};
-
-const enauData = {
-  translation: {
-    SKILL_NAME: 'Austrailian Foodoo',
-  },
-};
-
-const encaData = {
-  translation: {
-    SKILL_NAME: 'Canadian Foodoo',
-  },
-};
-
-const engbData = {
-  translation: {
-    SKILL_NAME: 'British Foodoo',
-  },
-};
-
-const eninData = {
-  translation: {
-    SKILL_NAME: 'Indian Foodoo',
-  },
-};
-
-const enusData = {
-  translation: {
-    SKILL_NAME: 'American Foodoo',
-  },
-};
-
-// constructs i18n and l10n data structure
-// translations for this sample can be found at the end of this file
-const languageStrings = {
-  'de': deData,
-  'de-DE': dedeData,
-  'en': enData,
-  'en-AU': enauData,
-  'en-CA': encaData,
-  'en-GB': engbData,
-  'en-IN': eninData,
-  'en-US': enusData,
-};
---
+++ lambda/custom/internationalization/languages/en.js
@@ -0,0 +1,56 @@
+const enData = {
+    translation: {
+        SKILL_NAME: 'Foodoo',
+        HELP_MESSAGE: 'You can say hi, or, you can say exit... What can I help you with?',
+        HELP_REPROMPT: 'What can I help you with?',
+        FALLBACK_MESSAGE: 'The Foodoo skill can\'t help you with that.  It can help you discover facts about space if you say tell me a space fact. What can I help you with?',
+        FALLBACK_REPROMPT: 'What can I help you with?',
+        ERROR_MESSAGE: 'Sorry, an error occurred.',
+        STOP_MESSAGE: 'Goodbye!',
+        HELLO_WORLD_MESSAGE:
+        [
+            'Hello World!',
+            'Hey World!',
+            'Hello my little World!',
+        ],
+    },
+};
+
+const enauData = {
+    translation: {
+        SKILL_NAME: 'Austrailian Foodoo',
+    },
+};
+
+const encaData = {
+    translation: {
+        SKILL_NAME: 'Canadian Foodoo',
+    },
+};
+
+const engbData = {
+    translation: {
+        SKILL_NAME: 'British Foodoo',
+    },
+};
+
+const eninData = {
+    translation: {
+        SKILL_NAME: 'Indian Foodoo',
+    },
+};
+
+const enusData = {
+    translation: {
+        SKILL_NAME: 'American Foodoo',
+    },
+};
+
+module.exports = {
+    enData,
+    enauData,
+    encaData,
+    engbData,
+    eninData,
+    enusData,
+}
